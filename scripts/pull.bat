8f48dabe2bed5ac7b2767a8dd6ed69d5859d6300
John Kidd <jwkidd3@Johns-MacBook-Air.local>
2025-10-03 12:36:12 -0500

    more

--- scripts/pull.bat
+++ scripts/pull.bat
@@ -1,3 +1,3 @@
 @echo off
-powershell -Command "Set-Location C:\Users\Administrator\splunk_fund_v3; Write-Host 'Pulling latest changes from repository...' -ForegroundColor Green; git pull; Write-Host 'Pull complete!' -ForegroundColor Green"
+powershell -Command "Set-Location C:\Users\Administrator\Desktop\splunk_fund_v3; Write-Host 'Pulling latest changes from repository...' -ForegroundColor Green; git pull; Write-Host 'Pull complete!' -ForegroundColor Green"
 pause
