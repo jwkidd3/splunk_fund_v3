e8c97cedc183441c7a2135b7a7ba5f865fda96fd
John Kidd <jwkidd3@Johns-MacBook-Air.local>
2025-10-03 11:54:17 -0500

    more

--- cleanup-splunk.bat
+++ cleanup-splunk.bat
@@ -1,19 +1,8 @@
 @echo off
 echo Removing Splunk container...
-
 docker rm -f splunk
-if %ERRORLEVEL% EQU 0 (
-    echo Splunk container removed successfully!
-) else (
-    echo Failed to remove Splunk container or container does not exist
-)
 
 echo Removing Splunk image...
 docker rmi splunk/splunk:latest
-if %ERRORLEVEL% EQU 0 (
-    echo Splunk image removed successfully!
-) else (
-    echo Failed to remove Splunk image
-)
 
 echo Cleanup complete!
