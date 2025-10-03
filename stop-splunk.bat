5fb7dbf376299ec6329c4545d179b3678618c56b
John Kidd <jwkidd3@Johns-MacBook-Air.local>
2025-10-03 12:22:10 -0500

    more

--- stop-splunk.bat
+++ stop-splunk.bat
@@ -1,17 +1,4 @@
 @echo off
 echo Stopping Splunk container...
-
 docker stop splunk
-if %ERRORLEVEL% EQU 0 (
-    echo Splunk container stopped successfully!
-) else (
-    echo Failed to stop Splunk container
-)
-
-echo Removing Splunk container...
-docker rm splunk
-if %ERRORLEVEL% EQU 0 (
-    echo Splunk container removed successfully!
-) else (
-    echo Failed to remove Splunk container
-)
+echo Splunk container stopped!
